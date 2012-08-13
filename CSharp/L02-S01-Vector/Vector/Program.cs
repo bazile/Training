0f531b790c5605d9322316aea554b6163a288343
Vasily <appdomain@yandex.ru>
2012-08-13 17:46:35 +0300

    Added foreach usage to L02-S01-Vector

--- CSharp/L02-S01-Vector/Vector/Program.cs
+++ CSharp/L02-S01-Vector/Vector/Program.cs
@@ -11,15 +11,20 @@ namespace MyVector
 			Vector v = new Vector(vectorLength);
 			//var v = new Vector(10);
 
-			// Заполняем вектор случайными числами от 70 до 450
+			// Заполняем вектор случайными числами от 1 до 999
 			System.Random random = new System.Random();
 			for (int i=0; i<vectorLength; i++)
 			{
-				v[i] = random.Next(70, 450);
+				v[i] = random.Next(1, 999);
 			}
 
 			System.Console.WriteLine("Length of vector = {0}", v.Length);
 			System.Console.WriteLine("Sum of vector = {0}", v.GetSum());
+
+			foreach (int item in v)
+			{
+				System.Console.WriteLine("{0:000}", item);
+			}
 		}
 	}
 }
