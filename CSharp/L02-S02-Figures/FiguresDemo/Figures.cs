48f4f5d89a915f0f8b3ea55373ae28592e3d3a5d
Vasily <appdomain@yandex.ru>
2012-08-15 16:10:13 +0300

    Updated Figures demo

--- CSharp/L02-S02-Figures/FiguresDemo/Figures.cs
+++ CSharp/L02-S02-Figures/FiguresDemo/Figures.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Демонстрация наследования, абстрактных классов, виртуальных и невиртуальных методов.
+ * Также демонстрируется использование класса System.Diagnostics.Debug
+*/
+
+using System;
+using System.Diagnostics;
 using System.Drawing;
 
 namespace FiguresDemo
@@ -6,39 +12,82 @@ namespace FiguresDemo
     public abstract class Figure
     {
 	    public abstract double GetArea();
+
+		public string WhoAmI()
+		{
+			return "Figure";
+		}
     }
 
 	public class Square : Figure
 	{
-		private Point _topLeft;
-		private double _size;
+		public Point TopLeft { get; private set; }
+		public double Size { get; private set; }
 
 		public Square(Point topLeft, double size)
 		{
-			_topLeft = topLeft;
-			_size = size;
+			TopLeft = topLeft;
+			Size = size;
 		}
 
 		public override double GetArea()
 		{
-			return _size*_size;
+			Debug.WriteLine("Square.GetArea()");
+			return Size*Size;
+		}
+
+		public new string WhoAmI()
+		{
+			return "Square";
 		}
 	}
 
-	public class Circle : Figure
+	public class Ellipse : Figure
 	{
-		private Point _center;
-		private double _radius;
+		public Point Center { get; private set; }
+		public double MajorRadius { get; private set; }
+		public double MinorRadius { get; private set; }
 
-		public Circle(Point center, double radius)
+		public Ellipse(Point center, double majorRadius, double minorRadius)
 		{
-			_center = center;
-			_radius = radius;
+			Center = center;
+			MajorRadius = majorRadius;
+			MinorRadius = minorRadius;
 		}
 
 		public override double GetArea()
 		{
-			return Math.PI*_radius*_radius;
+			Debug.WriteLine("Ellipse.GetArea()");
+			return Math.PI*MajorRadius*MinorRadius;
+		}
+
+		public new string WhoAmI()
+		{
+			return "Ellipse";
+		}
+	}
+
+	public class Circle : Ellipse
+	{
+		public double Radius
+		{
+			get { return MajorRadius; }
+		}
+
+		public Circle(Point center, double radius)
+			: base(center, radius, radius)
+		{
+		}
+
+		//public override double GetArea()
+		//{
+		//	Debug.WriteLine("Circle.GetArea()");
+		//	return Math.PI*Radius*Radius;
+		//}
+
+		public new string WhoAmI()
+		{
+			return "Circle";
 		}
 	}
 }
