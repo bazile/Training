f832b56ab4d045dd059844a7f1e8db91d9f1943b
Vasily <appdomain@yandex.ru>
2012-08-22 18:02:17 +0300

    Xml serialization sample

---
+++ CSharp/L06-S01-Xml/XmlSerialization.Sample03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace XmlSamples.Sample03
+{
+	public class Person
+	{
+		public string FirstName;
+		public string MiddleInitial;
+		public string LastName;
+
+		//[NonSerialized]
+		public string PlaceOfBirth;
+
+		private int _age = 420;
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person p = new Person {FirstName = "Duncan", MiddleInitial = "?", LastName = "MacLeod", PlaceOfBirth = "Glenfinnan, Scotland"};
+			//Person p = new Person();
+			//p.FirstName = "Jeff";
+			//p.MiddleInitial = "A";
+			//p.LastName = "Price";
+			//p.PlaceOfBirth = "Glenfinnan, Scotland";
+
+			XmlSerializer xmlSerializer = new XmlSerializer(p.GetType());
+			xmlSerializer.Serialize(Console.Out, p);
+			Console.WriteLine();
+		}
+	}
+}
