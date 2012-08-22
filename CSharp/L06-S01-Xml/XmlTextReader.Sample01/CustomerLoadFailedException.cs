316c502e5892cd9f1d38f06430d9a4922ad4ae4c
Vasily <appdomain@yandex.ru>
2012-08-23 01:13:02 +0300

    Exception handling in XML sample

---
+++ CSharp/L06-S01-Xml/XmlTextReader.Sample01/CustomerLoadFailedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace XmlSamples.Sample01
+{
+	[Serializable]
+	internal class CustomerLoadFailedException : ApplicationException
+	{
+		public CustomerLoadFailedException()
+		{
+		}
+
+		public CustomerLoadFailedException(string message) : base(message)
+		{
+		}
+
+		public CustomerLoadFailedException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+
+		protected CustomerLoadFailedException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
--- CSharp/L06-S01-Xml/XmlTextReader.Sample01/MainForm.cs
+++ CSharp/L06-S01-Xml/XmlTextReader.Sample01/MainForm.cs
@@ -23,9 +23,19 @@ namespace XmlSamples.Sample01
 			string customersXmlPath = Util.GetPathToCustomersXml();
 			string customersXsdPath = Util.GetPathToCustomersXsd();
 
-			List<Customer> customers = GetCustomersUsingXmlReader(customersXmlPath);
-			//List<Customer> customers = GetCustomersUsingXmlReader(customersXmlPath, customersXsdPath);
-			//List<Customer> customers = GetCustomersUsingXmlDocument(customersXmlPath);
+			List<Customer> customers;
+
+			try
+			{
+				//customers = GetCustomersUsingXmlReader(customersXmlPath);
+				//customers = GetCustomersUsingXmlDocument(customersXmlPath);
+				//customers = GetCustomersUsingXmlReader(customersXmlPath, customersXsdPath);
+				customers = GetCustomersUsingXmlDocument(customersXmlPath, customersXsdPath);
+			}
+			catch(CustomerLoadFailedException)
+			{
+				return;
+			}
 
 			DisplayCustomers(customers);
 		}
@@ -73,6 +83,7 @@ namespace XmlSamples.Sample01
 			finally
 			{
 				customersListView.EndUpdate();  // !!!
+				customersListView.EndUpdate();  // !!!
 			}
 		}
 
@@ -80,22 +91,28 @@ namespace XmlSamples.Sample01
 		/// Возвращает список клиентов прочитанный из указанного XML файла с помощью класса XmlDocument
 		/// </summary>
 		/// <param name="customersXmlPath">Путь к файлу customers.xml</param>
+		/// <param name="customersXsdPath">Путь к файлу customers.xsd</param>
 		/// <returns>Cписок клиентов</returns>
 		/// <remarks>Обратите внимание, что мы всегда возврашаем коллекцию даже если ничего не прочитали из файла</remarks>
-		private List<Customer> GetCustomersUsingXmlDocument(string customersXmlPath)
+		private List<Customer> GetCustomersUsingXmlDocument(string customersXmlPath, string customersXsdPath = null)
 		{
 			var customers = new List<Customer>();
 
 			var xmlDoc = new XmlDocument();
 			xmlDoc.Load(customersXmlPath);
+			if (customersXsdPath != null)
+			{
+				xmlDoc.Schemas.Add("http://tc.belhard.com/2012/Customers", customersXsdPath);
+				//xmlDoc.Validate(null);
+			}
 
 			XmlNodeList customerNodes = xmlDoc.DocumentElement.GetElementsByTagName("Customer");
 			foreach (XmlElement customerNode in customerNodes)
 			{
 				var customer = new Customer();
 				customer.CustomerId = customerNode.GetAttribute("CustomerId");
-				customer.CompanyName = customerNode.GetElementsByTagName("CompanyName")[0].InnerText;
-				customer.Country = customerNode.GetElementsByTagName("Country")[0].InnerText;
+				customer.CompanyName = customerNode.SelectSingleNode("./CompanyName").InnerText;
+				customer.Country = customerNode.SelectSingleNode("./FullAddress/Country").InnerText;
 
 				customers.Add(customer);
 			}
@@ -107,50 +124,10 @@ namespace XmlSamples.Sample01
 		/// Возвращает список клиентов прочитанный из указанного XML файла с помощью класса XmlTextReader
 		/// </summary>
 		/// <param name="customersXmlPath">Путь к файлу customers.xml</param>
+		/// <param name="customersXsdPath">Путь к файлу customers.xsd</param>
 		/// <returns>Cписок клиентов</returns>
 		/// <remarks>Обратите внимание, что мы всегда возврашаем коллекцию даже если ничего не прочитали из файла</remarks>
-		private List<Customer> GetCustomersUsingXmlReader(string customersXmlPath)
-		{
-			var customers = new List<Customer>();
-			using (XmlReader reader = XmlReader.Create(customersXmlPath))
-			{
-				while (reader.Read())
-				{
-					if (reader.NodeType == XmlNodeType.Element)
-					{
-						if (reader.Name == "Customer")
-						{
-							using (XmlReader innerReader = reader.ReadSubtree())
-							{
-								if (innerReader.Read())
-								{
-									var customer = new Customer();
-									customer.CustomerId = innerReader.GetAttribute("CustomerId");
-
-									innerReader.ReadToFollowing("CompanyName");
-									customer.CompanyName = innerReader.ReadElementContentAsString();
-
-									innerReader.ReadToFollowing("Country");
-									customer.Country = innerReader.ReadElementContentAsString();
-
-									customers.Add(customer);
-								}
-							}
-						} // if (reader.Name == "Customer")
-					} // if (reader.NodeType == XmlNodeType.Element)
-				}
-			}
-
-			return customers;
-		}
-
-		/// <summary>
-		/// Возвращает список клиентов прочитанный из указанного XML файла с помощью класса XmlTextReader
-		/// </summary>
-		/// <param name="customersXmlPath">Путь к файлу customers.xml</param>
-		/// <returns>Cписок клиентов</returns>
-		/// <remarks>Обратите внимание, что мы всегда возврашаем коллекцию даже если ничего не прочитали из файла</remarks>
-		private List<Customer> GetCustomersUsingXmlReader(string customersXmlPath, string customersXsdPath)
+		private List<Customer> GetCustomersUsingXmlReader(string customersXmlPath, string customersXsdPath = null)
 		{
 			var customers = new List<Customer>();
 			
@@ -161,34 +138,41 @@ namespace XmlSamples.Sample01
 				readerSettings.Schemas.Add("http://tc.belhard.com/2012/Customers", customersXsdPath);
 				readerSettings.ValidationType = ValidationType.Schema;
 			}
-			
-			using (var reader = XmlReader.Create(customersXmlPath, readerSettings))
+
+			try
 			{
-				while (reader.Read())
+				using (var reader = XmlReader.Create(customersXmlPath, readerSettings))
 				{
-					if (reader.NodeType == XmlNodeType.Element)
+					while (reader.Read())
 					{
-						if (reader.Name == "Customer")
+						if (reader.NodeType == XmlNodeType.Element)
 						{
-							using (XmlReader innerReader = reader.ReadSubtree())
+							if (reader.Name == "Customer")
 							{
-								if (innerReader.Read())
+								using (XmlReader innerReader = reader.ReadSubtree())
 								{
-									var customer = new Customer();
-									customer.CustomerId = innerReader.GetAttribute("CustomerId");
+									if (innerReader.Read())
+									{
+										var customer = new Customer();
+										customer.CustomerId = innerReader.GetAttribute("CustomerId");
 
-									innerReader.ReadToFollowing("CompanyName");
-									customer.CompanyName = innerReader.ReadElementContentAsString();
+										innerReader.ReadToFollowing("CompanyName");
+										customer.CompanyName = innerReader.ReadElementContentAsString();
 
-									innerReader.ReadToFollowing("Country");
-									customer.Country = innerReader.ReadElementContentAsString();
+										innerReader.ReadToFollowing("Country");
+										customer.Country = innerReader.ReadElementContentAsString();
 
-									customers.Add(customer);
+										customers.Add(customer);
+									}
 								}
-							}
-						} // if (reader.Name == "Customer")
-					} // if (reader.NodeType == XmlNodeType.Element)
+							} // if (reader.Name == "Customer")
+						} // if (reader.NodeType == XmlNodeType.Element)
+					}
 				}
+			}
+			catch(System.Xml.Schema.XmlSchemaValidationException ex)
+			{
+				throw new CustomerLoadFailedException("Customer.xml has some errors", ex);
 			}
 
 			return customers;
