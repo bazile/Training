184b3344cbeec9954e78cbe27674fbd3d4061367
Vasily <appdomain@yandex.ru>
2012-08-27 01:21:05 +0300

    Initial commit of Threads Demo

---
+++ CSharp/L08-S02-Threads/ThreadsDemo-01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadsDemo_01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Thread backThread = new Thread(new ThreadStart(PrintMessage));
+			Thread paramThread = new Thread(new ParameterizedThreadStart(PrintMessage));
+			backThread.Start();
+			paramThread.Start("I'm parameterized thread!");
+
+			for (int i = 0; i < 100; i++)
+			{
+				Console.WriteLine("I'm main thread!");
+			}
+
+			// Ожидаем завершения потоков
+			backThread.Join();
+			paramThread.Join();
+		}
+
+		public static void PrintMessage()
+		{
+			for (int i = 0; i < 100; i++)
+			{
+				Console.WriteLine("I'm background thread!");
+			}
+		}
+
+		public static void PrintMessage(object text)
+		{
+			for (int i = 0; i < 100; i++)
+			{
+				Console.WriteLine(text);
+			}
+		}
+	}
+}
---
+++ CSharp/L08-S02-Threads/ThreadsDemo-01/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ThreadsDemo-01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThreadsDemo-01")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
