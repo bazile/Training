a018fb4bc3b02cd25a1009ca99c8cfef0d0a4728
Vasily <appdomain@yandex.ru>
2012-12-27 14:43:01 +0300

    Added Singleton sample

---
+++ CSharp/L15-S01-DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Singleton
+{
+	class Program
+	{
+		static void Main()
+		{
+			// Экземпляры System.Type являются синглтонами
+			Type t1 = typeof(int);
+			Type t2 = Type.GetType("System.Int32");
+			Debug.Assert(Object.ReferenceEquals(t1, t2));
+		}
+	}
+}
