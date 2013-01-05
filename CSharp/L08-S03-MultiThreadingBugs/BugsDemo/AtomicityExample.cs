6f54909e776445ea1f670f36301e080ee86144f9
Vasily <appdomain@yandex.ru>
2013-01-05 20:13:46 +0300

    Added multi-threading bugs demo

---
+++ CSharp/L08-S03-MultiThreadingBugs/BugsDemo/AtomicityExample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BugsDemo
+{
+	internal class AtomicityExample
+	{
+		private Guid _value;
+
+		public void SetValue(Guid value)
+		{
+			_value = value;
+		}
+
+		public Guid GetValue()
+		{
+			return _value;
+		}
+	}
+}
---
+++ CSharp/L08-S03-MultiThreadingBugs/BugsDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿// ReSharper disable RedundantUsingDirective
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+// ReSharper restore RedundantUsingDirective
+
+namespace BugsDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Работа со списком из разных потоков
+
+			//var items = new List<int>();
+			//Parallel.For(0, 1000000, items.Add); // Вызываем потоко-небезопасный метод Add() из разных потоков без синхронизации
+			////Parallel.For(0, 1000000, i =>
+			////    {
+			////        lock (items)
+			////        {
+			////            items.Add(i);
+			////        }
+			////    });
+			//items.Add(0); // Ожидаем здесь исключение т.к. список находится в испорченном состоянии
+
+			#endregion
+
+			#region Неатомарная запись и чтение
+
+			var obj = new AtomicityExample();
+			const int runTimes = 10000000;
+			Task setGuidOfZero = Task.Factory.StartNew(() => Parallel.For(0, runTimes, i => obj.SetValue(Guid.Empty)));
+			Task setGuidOfFive = Task.Factory.StartNew(() => Parallel.For(0, runTimes, i => obj.SetValue(new Guid("{55555555-5555-5555-5555-555555555555}"))));
+			Task readAndPrintGuid = Task.Factory.StartNew(
+				() => Parallel.For(0, runTimes, 
+					i =>
+						{
+							Guid guid = obj.GetValue();
+							byte[] bytes = guid.ToByteArray();
+							for (int k=1; k<bytes.Length; k++)
+							{
+								if (bytes[k - 1] != bytes[k])
+								{
+									Console.WriteLine(guid);
+								}
+							}
+						}
+				)
+			);
+			Task.WaitAll(setGuidOfZero, setGuidOfFive, readAndPrintGuid);
+
+			#endregion
+		}
+	}
+}
