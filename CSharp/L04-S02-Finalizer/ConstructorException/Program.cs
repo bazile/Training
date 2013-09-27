6d90aa95fffff292027bb706ae1ddd82f04788ef
Vasily <appdomain@yandex.ru>
2013-09-27 15:30:54 +0300

    Added Finalizer sample

---
+++ CSharp/L04-S02-Finalizer/ConstructorException/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConstructorException
+{
+    class Program
+    {
+        static void Main()
+        {
+            Test t = null;
+            try
+            {
+                t = new Test();
+            }
+            catch
+            {
+                Console.WriteLine("Caugth exception");
+            }
+
+            // Конструктор сгенерировал исключение поэтому переменная будет равна null
+            Console.WriteLine(t == null ? "null" : "not null");
+
+            // Память под объект уже была выделена и т.к. у класса Test есть финализатор,
+            //  то CLR обязана его выполнить. Поэтому мы увидим на экране текст выводимый финализатором.
+        }
+    }
+
+    class Test
+    {
+        public Test()
+        {
+            throw new Exception();
+        }
+
+        ~Test()
+        {
+            Console.WriteLine("Finalizer");
+        }
+    }
+}
