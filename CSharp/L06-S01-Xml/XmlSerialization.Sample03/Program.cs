21b5d28bd582e387913452a3173db0d19c678e8e
Vasily <vasily.petruhin@gmail.com>
2013-11-12 11:31:13 +0300

    Changed Xml serialization example

--- CSharp/L06-S01-Xml/XmlSerialization.Sample03/Program.cs
+++ CSharp/L06-S01-Xml/XmlSerialization.Sample03/Program.cs
@@ -1,33 +1,62 @@
 ﻿using System;
+using System.IO;
 using System.Xml.Serialization;
 
 namespace XmlSamples.Sample03
 {
 	public class Person
 	{
+        // XML сериализация умеет работать только с public полями/свойствами
 		public string FirstName;
-		public string MiddleInitial;
 		public string LastName;
 
-		//[NonSerialized]
+        //[XmlIgnore]
 		public string PlaceOfBirth;
+        
+        // private поля не участвуют в XML сериализации
+		private int _age;
 
-		private int _age = 420;
+        public void SetAge(int age)
+        {
+            _age = age;
+        }
+
+	    public override string ToString()
+	    {
+	        return string.Format("{0} {1} from {2}. Age={3}", FirstName, LastName, PlaceOfBirth, _age);
+	    }
 	}
 
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-			Person p = new Person {FirstName = "Duncan", MiddleInitial = "?", LastName = "MacLeod", PlaceOfBirth = "Glenfinnan, Scotland"};
-			//Person p = new Person();
-			//p.FirstName = "Jeff";
-			//p.MiddleInitial = "A";
-			//p.LastName = "Price";
-			//p.PlaceOfBirth = "Glenfinnan, Scotland";
+			Person duncanMacLeod = new Person {FirstName = "Duncan", LastName = "MacLeod", PlaceOfBirth = "Glenfinnan, Scotland"};
+            duncanMacLeod.SetAge(420);
+            Console.WriteLine(duncanMacLeod);
 
-			XmlSerializer xmlSerializer = new XmlSerializer(p.GetType());
-			xmlSerializer.Serialize(Console.Out, p);
+            // Будем выполнять сериализацию в MemoryStream чтобы не создавать временный файл
+            MemoryStream memStream = new MemoryStream();
+
+            // Создаем экземпляр XmlSerializer для выполнения сериализации и десериализации
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+
+            // Сериализуем значение переменной duncanMacLeod
+            xmlSerializer.Serialize(memStream, duncanMacLeod);
+
+            // Читаем данные из потока чтобы увидеть результат сериализации
+		    memStream.Position = 0;
+            StreamReader reader = new StreamReader(memStream);
+		    string xmlResult = reader.ReadToEnd();
+            Console.WriteLine("[--------------------------------------------------]");
+		    Console.WriteLine(xmlResult);
+            Console.WriteLine("[--------------------------------------------------]");
+
+            // Десериализуем данные из потока
+		    memStream.Position = 0;
+		    Person person = (Person) xmlSerializer.Deserialize(memStream);
+            Console.WriteLine(person);
+
 			Console.WriteLine();
 		}
 	}
