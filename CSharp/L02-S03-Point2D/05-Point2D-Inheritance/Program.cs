5d24bf0acac802d1af434073d2ab4d794052788f
Vasily <vasily.petruhin@gmail.com>
2013-11-17 15:23:21 +0300

    Updated Point2D example

--- CSharp/L02-S03-Point2D/05-Point2D-Inheritance/Program.cs
+++ CSharp/L02-S03-Point2D/05-Point2D-Inheritance/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace BelhardTraining.PointDemo
+﻿using System;
+
+namespace BelhardTraining.PointDemo
 {
 	class Program
 	{
 		static void Main()
 		{
+			Point2D p1 = new Point2D(10,7);
+			Point3D p2 = new Point3D(10, 7, -5);
+			Point3D p3 = new Point3D(10, 7, 5);
+
+			// Переменная p1 имеет тип Point2D, p2 - Point3D
+			// Функция CalculateDistance из класса Point2D принимает аргумент типа Point2D
+			// Т.к. класс Point3D является наследником Point3D, то этот вызов является допустимым
+			// Фактически мы вычисляем растояние между двумя Point2D игнорируя координату Z
+			Console.WriteLine(p1.CalculateDistance(p2)); // Результат - 0
+
+			// Аналогично первому вызову
+			Console.WriteLine(p2.CalculateDistance(p1)); // Результат - 0
+
+			// Обе переменные имеют тип Point3D
+			// Вызывается функция CalculateDistance класса Point3D
+			Console.WriteLine(p2.CalculateDistance(p3)); // Результат - 10
 		}
 	}
 }
--- CSharp/L02-S03-Point2D/06-Point2D-Interfaces/Program.cs
+++ CSharp/L02-S03-Point2D/06-Point2D-Interfaces/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Threading;
 
@@ -8,6 +9,33 @@ namespace BelhardTraining.PointDemo
 	{
 		static void Main(string[] args)
 		{
+			// Пример сортировки.
+			// Класс List<T> способен отсортировать экземпляры Point2D благодаря наличиб реализации IComparable
+			List<Point2D> points = new List<Point2D>();
+			points.Add(new Point2D(5, 5));
+			points.Add(new Point2D(2, 2));
+			points.Add(new Point2D(4, 4));
+			points.Add(new Point2D(1, 1));
+			points.Add(new Point2D(3, 3));
+
+			Console.WriteLine("До сортировки:");
+			foreach (Point2D point in points)
+			{
+				Console.WriteLine("({0}, {1})", point.X, point.Y);
+			}
+			
+			points.Sort();
+			Console.WriteLine("После сортировки:");
+			foreach (Point2D point in points)
+			{
+				Console.WriteLine("({0}, {1})", point.X, point.Y);
+			}
+
+			Console.WriteLine("--------------");
+			Console.WriteLine("Пример форматированного вывода");
+
+			// Форматированный вывод используя собственную реализацию IFormattable
+			Console.WriteLine(Thread.CurrentThread.CurrentCulture.Name);
 			Point2D p1 = new Point2D(-1, -2);
 			Console.WriteLine("{0}", p1);
 			Console.WriteLine("{0:R}", p1);
@@ -15,8 +43,9 @@ namespace BelhardTraining.PointDemo
 			Console.WriteLine("{0:C}", p1);
 			Console.WriteLine();
 			
-			Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
-			Console.WriteLine("en-US");
+			// На форматированный вывод влияет текущая культура
+			Thread.CurrentThread.CurrentCulture = new CultureInfo("en-NZ");
+			Console.WriteLine(Thread.CurrentThread.CurrentCulture.Name);
 
 			Console.WriteLine("{0}", p1);
 			Console.WriteLine("{0:R}", p1);
