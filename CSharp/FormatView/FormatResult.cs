9fda3add392eb5214fa2cbfd249482c346166798
Vasily <vasily.petruhin@gmail.com>
2013-11-18 18:08:17 +0300

    Initial commit of FormatView

---
+++ CSharp/FormatView/FormatResult.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace FormatView
+{
+    public class FormatResult<T> where T : IFormattable 
+    {
+        public FormatResult(T val, string formatString) : this(val, formatString, CultureInfo.CurrentCulture)
+        {
+        }
+
+        public FormatResult(T val, string formatString, IFormatProvider formatProvider)
+        {
+            FormatString = formatString;
+            Result = val.ToString(formatString, formatProvider);
+        }
+
+        public string FormatString { get; private set; }
+        public string Result { get; private set; }
+        public string Comment { get; set; }
+    }
+
+
+    public class FormatStringInfo
+    {
+        public string[] Formats { get; set; }
+        public string Name { get; set; }
+    }
+
+    public interface IFormatStrings<T>
+    {
+        List<FormatStringInfo> GetFormatString();
+    }
+
+    public class FormatStrings : IFormatStrings<DateTime>, IFormatStrings<int>
+    {
+        private FormatStrings() { }
+
+        private static readonly FormatStrings _fs = new FormatStrings();
+
+        public static List<FormatStringInfo> GetFormatString<T>()
+        {
+            return ((IFormatStrings<T>) _fs).GetFormatString();
+        }
+
+        List<FormatStringInfo> IFormatStrings<DateTime>.GetFormatString()
+        {
+            //"f" Full date/time pattern (short time).
+            //"F" Full date/time pattern (long time).
+            //"g" General date/time pattern (short time).
+            //"G" General date/time pattern (long time).
+            //"M", "m" Month/day pattern.
+            //"O", "o" Round-trip date/time pattern.
+            //"R", "r" RFC1123 pattern.
+            //"s" Sortable date/time pattern.
+            //"t" Short time pattern.
+            //"T" Long time pattern.
+            //"u" Universal sortable date/time pattern.
+            //"U" Universal full date/time pattern.
+            //"Y", "y" Year month pattern.
+            return new List<FormatStringInfo>
+                {
+                    new FormatStringInfo{ Name = "Short date pattern"                    , Formats = new[] {"d"} },
+                    new FormatStringInfo{ Name = "Long date pattern"                     , Formats = new[] {"D"} },
+                    new FormatStringInfo{ Name = "Full date/time pattern (short time)"   , Formats = new []{"f"} },
+                    new FormatStringInfo{ Name = "Full date/time pattern (long time)"    , Formats = new []{"F"} },
+                    new FormatStringInfo{ Name = "General date/time pattern (short time)", Formats = new []{"g"} },
+                    new FormatStringInfo{ Name = "General date/time pattern (long time)" , Formats = new []{"G"} },
+                    new FormatStringInfo{ Name = "Month/day pattern"                     , Formats = new []{"M", "m"} },
+                    new FormatStringInfo{ Name = "Round-trip date/time pattern"          , Formats = new []{"O", "o"} },
+                    new FormatStringInfo{ Name = "RFC1123 pattern"                       , Formats = new []{"R", "r"} },
+                    new FormatStringInfo{ Name = "Sortable date/time pattern"            , Formats = new []{"s"} },
+                    new FormatStringInfo{ Name = "Short time pattern"                    , Formats = new []{"t"} },
+                    new FormatStringInfo{ Name = "Long time pattern"                     , Formats = new []{"T"} },
+                    new FormatStringInfo{ Name = "Universal sortable date/time pattern"  , Formats = new []{"u"} },
+                    new FormatStringInfo{ Name = "Universal full date/time pattern"      , Formats = new []{"U"} },
+                    new FormatStringInfo{ Name = "Year month pattern"                    , Formats = new []{"Y", "y"} },
+                };
+        }
+
+        List<FormatStringInfo> IFormatStrings<int>.GetFormatString()
+        {
+            return new List<FormatStringInfo>
+                {
+                    new FormatStringInfo{ Name = "Currency", Formats = new[] {"C", "c"}},
+                    new FormatStringInfo{ Name = "Decimal" , Formats = new[] {"D", "d"}},
+                };
+        }
+    }
+}
---
+++ CSharp/FormatView/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FormatView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ReadOnlyCollection<string> _numericFormats;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var numericFormats = new List<string> {"c", "d", "e", "f", "g", "n", "p", "x"};
+            _numericFormats = numericFormats.AsReadOnly();
+
+            var x = GetFormatResuls(-10);
+        }
+
+        private ReadOnlyCollection<FormatResult<int>> GetFormatResuls(int num)
+        {
+            var formatResults = new List<FormatResult<int>>();
+            foreach (string format in _numericFormats)
+            {
+                formatResults.Add(new FormatResult<int>(num, format));
+            }
+            return formatResults.AsReadOnly();
+        }
+
+        private void textBox1_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            listView1.BeginInit();
+            listView1.Items.Clear();
+            listView1.EndInit();
+        }
+
+        public List<FormatResult<DateTime>> results;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //string[] dateFormats = DateTime.Now.ToString()
+
+            DateTime d = DateTime.Now;
+            List<FormatStringInfo> dateFormats = FormatStrings.GetFormatString<DateTime>();
+            List<FormatResult<DateTime>> tempResults = new List<FormatResult<DateTime>>();
+            foreach (var fsi in dateFormats)
+            {
+                foreach (string format in fsi.Formats)
+                {
+                    var result = new FormatResult<DateTime>(d, format);
+                    result.Comment = fsi.Name;
+                    var lvi = new ListViewItem();
+                    lvi.Content = result;
+                    listView1.Items.Add(lvi);
+                    //tempResults.Add(result);
+                }
+            }
+
+            //listView1.DataContext = results;
+            //results = tempResults;
+        }
+    }
+}
