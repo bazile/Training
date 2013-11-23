4125abce6057730b0fcd3f17995b13bf9ed66398
Vasily <vasily.petruhin@gmail.com>
2013-11-24 00:52:41 +0300

    Removed _ prefix from Point2D demo

--- CSharp/L02-S03-Point2D/03-Point2D-And-Rectangle/Rectangle2D.cs
+++ CSharp/L02-S03-Point2D/03-Point2D-And-Rectangle/Rectangle2D.cs
@@ -4,34 +4,34 @@ namespace BelhardTraining.PointDemo
 {
     class Rectangle2D
     {
-        private int _width;
-        private int _height;
+        private int width;
+        private int height;
 
         public Point2D TopLeft { get; set; }
 
         public int Width
         {
-            get { return _width; }
+            get { return width; }
             set
             {
                 if (value <= 0)
                 {
                     throw new ArgumentOutOfRangeException("value", "Ширина должна быть положительным ненулевым числом.");
                 }
-                _width = value;
+                width = value;
             }
         }
 
         public int Height
         {
-            get { return _height; }
+            get { return height; }
             set
             {
                 if (value <= 0)
                 {
                     throw new ArgumentOutOfRangeException("value", "Высота должна быть положительным ненулевым числом.");
                 }
-                _height = value;
+                height = value;
             }
         }
 
