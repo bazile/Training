777c63fbc9aef54db533ecdfa16f97f997ab5537
Vasily <vasily.petruhin@gmail.com>
2013-11-28 20:44:59 +0300

    Added FamousQuote task and tests for it

---
+++ Homework-Tests/FamousQuote/FamousQuote.Tests/FamousQuoteFixture.cs
@@ -0,0 +1,34 @@
+﻿using BelhardTraining.FamousQuote.Service;
+using NUnit.Framework;
+
+namespace FamousQuote.Tests
+{
+    [TestFixture]
+    public class FamousQuoteFixture
+    {
+        private IFamousQuote _svc;
+
+        [SetUp]
+        public void TestSetUp()
+        {
+            _svc = new FamousQuoteService();
+        }
+
+        [Test]
+        public void ReturnsNull()
+        {
+            string text = _svc.FindQuoteByAuthor("Марк Твен");
+            Assert.IsNull(text);
+        }
+
+        public void ReturnsQuoteByAuthorName()
+        {
+            const string text = "Люк, я твой отец";
+            const string author = "Дарт Вейдер";
+            _svc.AddQuote(text, author);
+
+            string text2 = _svc.FindQuoteByAuthor(author);
+            Assert.AreEqual(text, text2);
+        }
+    }
+}
---
+++ Homework-Tests/FamousQuote/FamousQuoteHost/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ServiceModel;
+using BelhardTraining.FamousQuote.Service;
+
+namespace BelhardTraining.FamousQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseAddress = new Uri("http://localhost:1234/SingleQuote");
+            var serviceType = typeof(FamousQuoteService);
+            var binding = new BasicHttpBinding();
+            var host = new ServiceHost(serviceType, baseAddress);
+            //host.Description.Endpoints.Clear();//This line is added for convenience (and allows you to leave the app.config as-is.
+            host.AddServiceEndpoint(typeof(IFamousQuote), binding, "SingleQuote");
+
+            host.Open();
+            Console.WriteLine(baseAddress);
+            Console.WriteLine("Сервис запущен: Нажмите <Ввод> для выхода");
+            Console.ReadLine();
+        }
+    }
+}
