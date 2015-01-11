4cbacfea2e9f37aad2e7d0fd131360654acf7a3e
Vasily <vasily.petruhin@gmail.com>
2015-01-11 18:56:52 +0300

    Use al.exe for .NET 4.x

--- CSharp/L07-S01-Modules/compile-al.cmd
+++ CSharp/L07-S01-Modules/compile-al.cmd
@@ -3,9 +3,9 @@ SETLOCAL
 
 SET "cscPath=%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe"
 IF EXIST "%ProgramFiles(x86)%" (
-	SET "alPath=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\al.exe"
+	SET "alPath=%ProgramFiles(x86)%\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\al.exe"
 ) ELSE (
-	SET "alPath=%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\Bin\al.exe"
+	SET "alPath=%ProgramFiles%\Microsoft SDKs\Windows\v7.0A\Bin\NETFX 4.0 Tools\al.exe"
 )
 
 REM Compile modules ignoring warnings about not used types and fields
