e2b5273928dcc330309c6d3d9a57531352162fb6
Vasily <vasily.petruhin@gmail.com>
2015-07-24 18:10:13 +0300

    Added Control.Invoke() deadlock demo

---
+++ CSharp/L08-S03-MultiThreadingBugs/ControlInvokeDeadlock/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace BelhardTraining.ControlInvokeDeadlock
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.bigButton = new System.Windows.Forms.Button();
+			this.statusLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// bigButton
+			// 
+			this.bigButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.bigButton.Location = new System.Drawing.Point(105, 76);
+			this.bigButton.Name = "bigButton";
+			this.bigButton.Size = new System.Drawing.Size(175, 137);
+			this.bigButton.TabIndex = 0;
+			this.bigButton.Text = "Нажми меня!";
+			this.bigButton.UseVisualStyleBackColor = true;
+			this.bigButton.Click += new System.EventHandler(this.bigButton_Click);
+			// 
+			// statusLabel
+			// 
+			this.statusLabel.AutoSize = true;
+			this.statusLabel.Location = new System.Drawing.Point(170, 50);
+			this.statusLabel.Margin = new System.Windows.Forms.Padding(10);
+			this.statusLabel.Name = "statusLabel";
+			this.statusLabel.Size = new System.Drawing.Size(35, 13);
+			this.statusLabel.TabIndex = 1;
+			this.statusLabel.Text = "label1";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(384, 262);
+			this.Controls.Add(this.statusLabel);
+			this.Controls.Add(this.bigButton);
+			this.Name = "MainForm";
+			this.Text = "Демонстрация взаимоблокировки";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button bigButton;
+		private System.Windows.Forms.Label statusLabel;
+	}
+}
+
---
+++ CSharp/L08-S03-MultiThreadingBugs/ControlInvokeDeadlock/MainForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+/*
+ * Демонстрация взаимоблокировки при неправильном использовании Control.Invoke
+ */
+
+#region Как исправить эту ошибку?
+
+// Вызов _unlocked.Set() должен стоять перед Invoke(...)
+// Таки образом UI поток закончит выполнение обработчика события Click
+//	и сможет обрабатывать сообщения от операцинной системы
+
+#endregion
+
+namespace BelhardTraining.ControlInvokeDeadlock
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		// Создаем событие в несигнализированном состоянии
+		ManualResetEvent _unlocked = new ManualResetEvent(false);
+
+		private void bigButton_Click(object sender, EventArgs e)
+		{
+			Thread deadlockThread = new Thread(Deadlock);
+			deadlockThread.Name = "Мой поток";
+			deadlockThread.Start();
+
+			statusLabel.Text = "Взаимоблокировка :(";
+			
+			// Ждем перехода события в сигнальное состояние
+			// Это никогда не произойдет т.к. событие переводится в сигнальное состояние
+			//	после вызова Control.Invoke который ждет когда UI поток освободится который 
+			//	ждет сигнала от второго потока который ждет UI поток который ....
+			// То из-за неправильного взаимодействия потоков мы получили взаимоблокировку
+			_unlocked.WaitOne();
+		}
+
+		void Deadlock()
+		{
+			// Пытаемся обновить UI. Метод Invoke блокирует исполнение текущего потока 
+			//	пока UI поток не выполнит запрошенное действие
+			//	Однако это никогда не прозойдет т.к. UI поток заблокирован
+			//	вызовом _unlocked.WaitOne() и не может обрабатывать сообщения из очереди
+			Invoke((Action)delegate() {
+				statusLabel.Text = "Избежали взаимоблокировки!";
+			});
+			
+			// Переводим событие в сигнальное состояние
+			// Следующая строка никогда не выполнится
+			_unlocked.Set();
+		}
+	}
+}
