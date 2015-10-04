bed2214a49034853d4daba304aea2ac3513773fa
Vasily <vasily.petruhin@gmail.com>
2015-10-04 03:14:37 +0300

    Added LogicalStringComparer

---
+++ CSharp/L04-S03-IO/DrivesFoldersFiles/LogicalStringComparer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace BelhardTraining.LessonIO
+{
+	/// <summary>
+	/// Сортировка строк в логическом порядке. Например строки
+	///		1.txt
+	///		10.txt
+	///		2.txt
+	/// будут отсортированы в
+	///		1.txt
+	///		2.txt
+	///		10.txt
+	/// </summary>
+	public class LogicalStringComparer : IComparer<string>
+	{
+		[DllImport("shlwapi.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
+		static extern int StrCmpLogicalW(string x, string y);
+
+		public int Compare(string x, string y)
+		{
+			return StrCmpLogicalW(x, y);
+		}
+	}
+}
