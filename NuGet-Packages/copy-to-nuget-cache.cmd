c6419825dd6fb9f7d93fe6a05cd52084561bf6a7
Vasily <vasily.petruhin@gmail.com>
2015-11-19 02:22:50 +0300

    Create NuGet cache folder if absent

--- NuGet-Packages/copy-to-nuget-cache.cmd
+++ NuGet-Packages/copy-to-nuget-cache.cmd
@@ -1,3 +1,5 @@
 @echo off
-IF NOT EXIST %LOCALAPPDATA%\NuGet\Cache GOTO :eof
+IF NOT EXIST "%LOCALAPPDATA%" GOTO :eof
+IF NOT EXIST "%LOCALAPPDATA%\NuGet\Cache" mkdir "%LOCALAPPDATA%\NuGet\Cache"
 copy "%~dp0*.nupkg" "%LOCALAPPDATA%\NuGet\Cache"
+pause
