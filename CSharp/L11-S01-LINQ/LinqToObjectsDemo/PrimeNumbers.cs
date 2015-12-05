a825ab839f6a90e11f119f63d68f0805a729902d
Vasily <vasily.petruhin@gmail.com>
2015-12-06 01:35:45 +0300

    Renamed methods for prime calculation

--- CSharp/L11-S01-LINQ/LinqToObjectsDemo/PrimeNumbers.cs
+++ CSharp/L11-S01-LINQ/LinqToObjectsDemo/PrimeNumbers.cs
@@ -4,35 +4,41 @@ using System.Linq;
 
 namespace BelhardTraining.LinqToObjectsDemo
 {
-	public static class PrimeNumbers
-	{
-		/// <summary>
-		/// Получение последовательности случайных чисел до указанного значения
-		/// </summary>
-		/// <param name="maxValue"></param>
-		public static IEnumerable<int> PrintPrimeNumbers(int maxValue)
-		{
-			Func<int, IEnumerable<int>> primeNumbers = max =>
-				 from i in Enumerable.Range(2, max - 1)
-				 where Enumerable.Range(2, i - 2).All(j => i % j != 0)
-				 select i;
+    public static class PrimeNumbers
+    {
+        /// <summary>
+        /// Получение последовательности случайных чисел до указанного значения
+        /// </summary>
+        /// <param name="maxValue"></param>
+        public static IEnumerable<int> GetPrimeNumbers(int maxValue)
+        {
+            if (maxValue == 1) return Enumerable.Empty<int>();
+            if (maxValue == 2) return new int[] {2};
 
-			return primeNumbers(maxValue);
-		}
+            Func<int, IEnumerable<int>> primeNumbers = max =>
+                 from i in Enumerable.Range(2, max - 1)
+                 where Enumerable.Range(2, (int)Math.Sqrt(maxValue)).All(j => i % j != 0)
+                 select i;
 
-		/// <summary>
-		/// Получение последовательности случайных чисел до указанного значения
-		///     с помощью Parallel LINQ
-		/// </summary>
-		/// <param name="maxValue"></param>
-		public static IEnumerable<int> PrintPrimeNumbersParallel(int maxValue)
-		{
-			Func<int, IEnumerable<int>> primeNumbers = max =>
-				 from i in Enumerable.Range(2, max - 1).AsParallel()
-				 where Enumerable.Range(2, i - 2).All(j => i % j != 0)
-				 select i;
+            return primeNumbers(maxValue);
+        }
 
-			return primeNumbers(maxValue);
-		}
-	}
+        /// <summary>
+        /// Получение последовательности случайных чисел до указанного значения
+        ///     с помощью Parallel LINQ
+        /// </summary>
+        /// <param name="maxValue"></param>
+        public static IEnumerable<int> GetPrimeNumbersParallel(int maxValue)
+        {
+            if (maxValue == 1) return Enumerable.Empty<int>();
+            if (maxValue == 2) return new int[] { 2 };
+
+            Func<int, IEnumerable<int>> primeNumbers = max =>
+                 from i in Enumerable.Range(2, max - 1).AsParallel()
+                 where Enumerable.Range(2, (int)Math.Sqrt(maxValue)).All(j => i % j != 0)
+                 select i;
+
+            return primeNumbers(maxValue);
+        }
+    }
 }
--- CSharp/L11-S01-LINQ/LinqToObjectsDemo/Program.cs
+++ CSharp/L11-S01-LINQ/LinqToObjectsDemo/Program.cs
@@ -6,190 +6,194 @@ using System.Linq;
 
 namespace BelhardTraining.LinqToObjectsDemo
 {
-	class Program
-	{
-		static void Main()
-		{
-			#region Поиск файлов без LINQ и с ним
+    class Program
+    {
+        static void Main()
+        {
+            #region Поиск файлов без LINQ и с ним
 
-			//string mydocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-			//ExtensioInfo[] extensions1 = GetExtensions(mydocuments);
-			//ExtensioInfo[] extensions2 = GetExtensionsUsingLinq(mydocuments);
-			//foreach (ExtensioInfo extensioInfo in extensions1)
-			//{
-			//    Console.WriteLine("{0} - {1}", extensioInfo.Count.ToString().PadRight(4), extensioInfo.Extension);
-			//}
-			//Console.WriteLine();
+            //string mydocuments = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            //ExtensioInfo[] extensions1 = GetExtensions(mydocuments);
+            //ExtensioInfo[] extensions2 = GetExtensionsUsingLinq(mydocuments);
+            //foreach (ExtensioInfo extensioInfo in extensions1)
+            //{
+            //    Console.WriteLine("{0} - {1}", extensioInfo.Count.ToString().PadRight(4), extensioInfo.Extension);
+            //}
+            //Console.WriteLine();
 
-			//// Убеждаемся что последовательности совпадают
-			//bool resultsAreEqual = extensions1.SequenceEqual(extensions2, new ExtensioInfoEqualityComparer());
-			//if (resultsAreEqual)
-			//{
-			//    WriteColorLine(ConsoleColor.Green, "Ура! Результат работы функций c LINQ и без него совпадают!");
-			//}
-			//else
-			//{
-			//    // Такого быть не должно!
-			//    WriteColorLine(ConsoleColor.Red, "Результат работы функций c LINQ и без него не совпадают! :(");
-			//}
+            //// Убеждаемся что последовательности совпадают
+            //bool resultsAreEqual = extensions1.SequenceEqual(extensions2, new ExtensioInfoEqualityComparer());
+            //if (resultsAreEqual)
+            //{
+            //    WriteColorLine(ConsoleColor.Green, "Ура! Результат работы функций c LINQ и без него совпадают!");
+            //}
+            //else
+            //{
+            //    // Такого быть не должно!
+            //    WriteColorLine(ConsoleColor.Red, "Результат работы функций c LINQ и без него не совпадают! :(");
+            //}
 
-			#endregion
+            #endregion
 
-			#region Простые числа
+            #region Простые числа
 
-			//const int maxValue = int.MaxValue/10000;
-			//var seq1 = PrimeNumbers.PrintPrimeNumbers(maxValue);
-			//var seq2 = PrimeNumbers.PrintPrimeNumbersParallel(maxValue);
-			//Stopwatch watch = Stopwatch.StartNew();
-			//int count1 = seq1.Count();
-			//watch.Stop();
-			//Console.WriteLine("Время без PLINQ: {0}", watch.Elapsed);
-			//watch = Stopwatch.StartNew();
-			//int count2 = seq2.Count();
-			//watch.Stop();
-			//Console.WriteLine("Время  с  PLINQ: {0}", watch.Elapsed);
+            //const int maxValue = int.MaxValue / 1000;
+            //var seq1 = PrimeNumbers.GetPrimeNumbers(maxValue);
+            //var seq2 = PrimeNumbers.GetPrimeNumbersParallel(maxValue);
 
-			//if (count1 != count2)
-			//{
-			//    throw new InvalidOperationException("Кол-во чисел не совпадает. Проверьте алгоритм.");
-			//}
+            //Console.Write("Время без PLINQ: ");
+            //Stopwatch watch = Stopwatch.StartNew();
+            //int count1 = seq1.Count();
+            //watch.Stop();
+            //Console.WriteLine(watch.Elapsed);
 
-			#endregion
-		}
+            //Console.Write("Время  с  PLINQ: ");
+            //watch = Stopwatch.StartNew();
+            //int count2 = seq2.Count();
+            //watch.Stop();
+            //Console.WriteLine(watch.Elapsed);
 
-		static void WriteColorLine(ConsoleColor foregroundColor, string message)
-		{
-			ConsoleColor oldColor = Console.ForegroundColor;
-			Console.ForegroundColor = foregroundColor;
-			Console.WriteLine(message);
-			Console.ForegroundColor = oldColor;
-		}
+            //if (count1 != count2)
+            //{
+            //    throw new InvalidOperationException("Кол-во чисел не совпадает. Проверьте алгоритм.");
+            //}
 
-		#region Поиск файлов без LINQ и с ним
+            #endregion
+        }
 
-		class ExtensioInfo
-		{
-			public string Extension;
-			public int Count;
-		}
+        static void WriteColorLine(ConsoleColor foregroundColor, string message)
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            Console.ForegroundColor = foregroundColor;
+            Console.WriteLine(message);
+            Console.ForegroundColor = oldColor;
+        }
 
-		class ExtensioInfoEqualityComparer : IEqualityComparer<ExtensioInfo>
-		{
-			public bool Equals(ExtensioInfo a, ExtensioInfo b)
-			{
-				return a.Count == b.Count && a.Extension == b.Extension;
-			}
+        #region Поиск файлов без LINQ и с ним
 
-			public int GetHashCode(ExtensioInfo info)
-			{
-				return info.Count.GetHashCode() ^ info.Extension.GetHashCode();
-			}
-		}
+        class ExtensioInfo
+        {
+            public string Extension;
+            public int Count;
+        }
 
-		static ExtensioInfo[] GetExtensions(string path)
-		{
-			// Собираем информацию о файлах
-			var extCount = new Dictionary<string, int>();
-			foreach (string fileName in SafeEnumerateFiles(path, "*.*", SearchOption.AllDirectories))
-			{
-				string extension = Path.GetExtension(fileName).ToLowerInvariant();
-				if (extCount.ContainsKey(extension))
-				{
-					extCount[extension]++;
-				}
-				else
-				{
-					extCount.Add(extension, 1);
-				}
-			}
+        class ExtensioInfoEqualityComparer : IEqualityComparer<ExtensioInfo>
+        {
+            public bool Equals(ExtensioInfo a, ExtensioInfo b)
+            {
+                return a.Count == b.Count && a.Extension == b.Extension;
+            }
 
-			// Копируем данные из хеш таблицы в массив и сортируем его
-			ExtensioInfo[] extensions = new ExtensioInfo[extCount.Count];
-			int idx = 0;
-			foreach (KeyValuePair<string, int> entry in extCount)
-			{
-				extensions[idx++] = new ExtensioInfo { Extension = entry.Key, Count = entry.Value };
-			}
-			Array.Sort(
-				extensions,
-				(inf1, inf2) => inf2.Count - inf1.Count != 0 ? inf2.Count - inf1.Count : inf1.Extension.CompareTo(inf2.Extension)
-			);
+            public int GetHashCode(ExtensioInfo info)
+            {
+                return info.Count.GetHashCode() ^ info.Extension.GetHashCode();
+            }
+        }
 
-			return extensions;
-		}
+        static ExtensioInfo[] GetExtensions(string path)
+        {
+            // Собираем информацию о файлах
+            var extCount = new Dictionary<string, int>();
+            foreach (string fileName in SafeEnumerateFiles(path, "*.*", SearchOption.AllDirectories))
+            {
+                string extension = Path.GetExtension(fileName).ToLowerInvariant();
+                if (extCount.ContainsKey(extension))
+                {
+                    extCount[extension]++;
+                }
+                else
+                {
+                    extCount.Add(extension, 1);
+                }
+            }
 
-		static ExtensioInfo[] GetExtensionsUsingLinq(string path)
-		{
-			return (
-					from fileName in SafeEnumerateFiles(path, "*.*", SearchOption.AllDirectories)
-					group fileName by Path.GetExtension(fileName).ToLowerInvariant() into ext
-					orderby ext.Count() descending, ext.Key
-					select new ExtensioInfo { Extension = ext.Key, Count = ext.Count() }
-				).ToArray();
-		}
+            // Копируем данные из хеш таблицы в массив и сортируем его
+            ExtensioInfo[] extensions = new ExtensioInfo[extCount.Count];
+            int idx = 0;
+            foreach (KeyValuePair<string, int> entry in extCount)
+            {
+                extensions[idx++] = new ExtensioInfo { Extension = entry.Key, Count = entry.Value };
+            }
+            Array.Sort(
+                extensions,
+                (inf1, inf2) => inf2.Count - inf1.Count != 0 ? inf2.Count - inf1.Count : inf1.Extension.CompareTo(inf2.Extension)
+            );
 
-		
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="path">Путь</param>
-		/// <param name="searchPattern">Маска поиска для файлов</param>
-		/// <param name="searchOption"></param>
-		/// <returns></returns>
-		private static IEnumerable<string> SafeEnumerateFiles(string path, string searchPattern, SearchOption searchOption)
-		{
-			Stack<string> dirs = new Stack<string>();
+            return extensions;
+        }
 
-			//if (!Directory.Exists(path))
-			//{
-			//    throw new DirectoryNotFoundException();
-			//}
-			dirs.Push(path);
+        static ExtensioInfo[] GetExtensionsUsingLinq(string path)
+        {
+            return (
+                    from fileName in SafeEnumerateFiles(path, "*.*", SearchOption.AllDirectories)
+                    group fileName by Path.GetExtension(fileName).ToLowerInvariant() into ext
+                    orderby ext.Count() descending, ext.Key
+                    select new ExtensioInfo { Extension = ext.Key, Count = ext.Count() }
+                ).ToArray();
+        }
 
-			while (dirs.Count > 0)
-			{
-				string currentDirPath = dirs.Pop();
-				if (searchOption == SearchOption.AllDirectories)
-				{
-					try
-					{
-						string[] subDirs = Directory.GetDirectories(currentDirPath);
-						foreach (string subDirPath in subDirs)
-						{
-							dirs.Push(subDirPath);
-						}
-					}
-					catch (UnauthorizedAccessException)
-					{
-						continue;
-					}
-					catch (DirectoryNotFoundException)
-					{
-						continue;
-					}
-				}
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path">Путь</param>
+        /// <param name="searchPattern">Маска поиска для файлов</param>
+        /// <param name="searchOption"></param>
+        /// <returns></returns>
+        private static IEnumerable<string> SafeEnumerateFiles(string path, string searchPattern, SearchOption searchOption)
+        {
+            Stack<string> dirs = new Stack<string>();
 
-				string[] files;
-				try
-				{
-					files = Directory.GetFiles(currentDirPath, searchPattern);
-				}
-				catch (UnauthorizedAccessException)
-				{
-					continue;
-				}
-				catch (DirectoryNotFoundException)
-				{
-					continue;
-				}
+            //if (!Directory.Exists(path))
+            //{
+            //    throw new DirectoryNotFoundException();
+            //}
+            dirs.Push(path);
 
-				foreach (string filePath in files)
-				{
-					yield return filePath;
-				}
-			}
-		}
+            while (dirs.Count > 0)
+            {
+                string currentDirPath = dirs.Pop();
+                if (searchOption == SearchOption.AllDirectories)
+                {
+                    try
+                    {
+                        string[] subDirs = Directory.GetDirectories(currentDirPath);
+                        foreach (string subDirPath in subDirs)
+                        {
+                            dirs.Push(subDirPath);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        continue;
+                    }
+                    catch (DirectoryNotFoundException)
+                    {
+                        continue;
+                    }
+                }
 
-		#endregion
-	}
+                string[] files;
+                try
+                {
+                    files = Directory.GetFiles(currentDirPath, searchPattern);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    continue;
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    continue;
+                }
+
+                foreach (string filePath in files)
+                {
+                    yield return filePath;
+                }
+            }
+        }
+
+        #endregion
+    }
 }
