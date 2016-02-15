92e89df61cbd3afd35c16cad356618da1af251a0
Vasily <vasily.petruhin@gmail.com>
2016-02-16 02:59:22 +0300

    Fixed prime calculation bug

--- CSharp/L11-S01-LINQ/LinqToObjectsDemo/PrimeNumbers.cs
+++ CSharp/L11-S01-LINQ/LinqToObjectsDemo/PrimeNumbers.cs
@@ -13,11 +13,11 @@ namespace BelhardTraining.LinqToObjectsDemo
         public static IEnumerable<int> GetPrimeNumbers(int maxValue)
         {
             if (maxValue == 1) return Enumerable.Empty<int>();
-            if (maxValue == 2) return new int[] {2};
+            if (maxValue == 2) return new[] {2};
 
             Func<int, IEnumerable<int>> primeNumbers = max =>
                  from i in Enumerable.Range(2, max - 1)
-                 where Enumerable.Range(2, (int)Math.Sqrt(maxValue)).All(j => i % j != 0)
+                 where Enumerable.Range(2, (int)Math.Sqrt(i)).All(j => i % j != 0)
                  select i;
 
             return primeNumbers(maxValue);
@@ -31,11 +31,11 @@ namespace BelhardTraining.LinqToObjectsDemo
         public static IEnumerable<int> GetPrimeNumbersParallel(int maxValue)
         {
             if (maxValue == 1) return Enumerable.Empty<int>();
-            if (maxValue == 2) return new int[] { 2 };
+            if (maxValue == 2) return new[] { 2 };
 
             Func<int, IEnumerable<int>> primeNumbers = max =>
                  from i in Enumerable.Range(2, max - 1).AsParallel()
-                 where Enumerable.Range(2, (int)Math.Sqrt(maxValue)).All(j => i % j != 0)
+                 where Enumerable.Range(2, (int)Math.Sqrt(i)).All(j => i % j != 0)
                  select i;
 
             return primeNumbers(maxValue);
