ffb15b72a1dff8e8d103c4a24810fb8056e6ddb3
Vasily <vasily.petruhin@gmail.com>
2016-04-14 23:54:49 +0300

    Updated copyright year in assemblyinfo.cs files

--- CSharp/L07-S01-Modules/AssemblyInfo.cs
+++ CSharp/L07-S01-Modules/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 
 [assembly: AssemblyTitle("MyAssembly")]
 [assembly: AssemblyDescription("MyAssembly description")]
--- CSharp/L08-S04-SingleInstanceApp/Mutex/Properties/AssemblyInfo.cs
+++ CSharp/L08-S04-SingleInstanceApp/Mutex/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Центр обучающих технологий \u00ABБелхард\u00BB")]
 [assembly: AssemblyProduct("Основы программирования на С#")]
-[assembly: AssemblyCopyright("Copyright © 2012-2014 Vasily Petruhin")]
+[assembly: AssemblyCopyright("Copyright \u00a9 2012-2016 Vasily Petruhin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
