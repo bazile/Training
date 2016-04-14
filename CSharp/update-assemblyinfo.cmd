443befc2dc3a5bd300e7dc3ce6d951af527171a0
Vasily <vasily.petruhin@gmail.com>
2016-04-14 23:53:54 +0300

    Script to update assemblyinfo.cs

---
+++ CSharp/update-assemblyinfo.cmd
@@ -0,0 +1,2 @@
+@echo off
+powershell -noprofile -ExecutionPolicy unrestricted -Command "Get-ChildItem -Recurse -Include AssemblyInfo.cs | ForEach-Object { $t = Get-Content $_.FullName; $t = $t -Replace('Copyright \u00a9 \d{4}-\d{4}', 'Copyright \u00a9 2012-2016'); Write-Host $_.FullName; Set-Content -Path $_.FullName -Value $t -Encoding UTF8; }"
