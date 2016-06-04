d3545ff9ae317ab8259725b7772ea8baa6cd6b2e
Vasily <vasily.petruhin@gmail.com>
2016-06-04 07:52:32 +0300

    Print NuGet cache path

--- NuGet-Packages/copy-to-nuget-cache.cmd
+++ NuGet-Packages/copy-to-nuget-cache.cmd
@@ -1,5 +1,9 @@
 @echo off
 IF NOT EXIST "%LOCALAPPDATA%" GOTO :eof
 IF NOT EXIST "%LOCALAPPDATA%\NuGet\Cache" mkdir "%LOCALAPPDATA%\NuGet\Cache"
+
+echo NuGet cache path: %LOCALAPPDATA%\NuGet\Cache
+echo.
+
 copy "%~dp0*.nupkg" "%LOCALAPPDATA%\NuGet\Cache"
 pause
