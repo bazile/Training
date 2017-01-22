b5007587d89bfa5236a3cad6029ef9300e07a2f4
Vasily <vasily.petruhin@gmail.com>
2017-01-22 16:54:50 +0300

    Updated copyright information

--- CSharp/CultureExplorer/CultureExplorer.WinForms/Properties/AssemblyInfo.cs
+++ CSharp/CultureExplorer/CultureExplorer.WinForms/Properties/AssemblyInfo.cs
@@ -3,9 +3,9 @@
 [assembly: AssemblyTitle("CultureExplorer")]
 [assembly: AssemblyDescription(".NET Culture Explorer using Windows Forms")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Центр обучающих технологий \u00ABБелхард\u00BB")]
+[assembly: AssemblyCompany("Центр Обучающих Технологий")]
 [assembly: AssemblyProduct("Основы программирования на С#")]
-[assembly: AssemblyCopyright("Copyright \u00a9 2012-2016 Vasily Petruhin")]
+[assembly: AssemblyCopyright("Copyright \u00a9 2012-2017 Vasily Petruhin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- CSharp/wpf-ObservableObject/wpf-ObservableObject40/Properties/AssemblyInfo.cs
+++ CSharp/wpf-ObservableObject/wpf-ObservableObject40/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Windows;
 [assembly: AssemblyTitle("wpf-ObservableObject40")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Центр обучающих технологий \u00ABБелхард\u00BB")]
+[assembly: AssemblyCompany("Центр Обучающих Технологий")]
 [assembly: AssemblyProduct("Основы программирования на С#")]
-[assembly: AssemblyCopyright("Copyright \u00a9 2012-2016 Vasily Petruhin")]
+[assembly: AssemblyCopyright("Copyright \u00a9 2012-2017 Vasily Petruhin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
