91af33614458b1a3bffe2661aeafffdda6cce6e7
Vasily <vasily.petruhin@gmail.com>
2017-09-03 14:50:48 +0300

    VS 2013 settings

---
+++ Visual Studio Settings/2013/apply-settings.cmd
@@ -0,0 +1,3 @@
+start /wait settings-vs2013.reg
+powershell -executionpolicy unrestricted .\settings-vs2013.ps1
+pause
---
+++ Visual Studio Settings/2013/settings-vs2013.ps1
@@ -0,0 +1,14 @@
+﻿$settingsPath = [environment]::getfolderpath(“mydocuments”) + "\Visual Studio 2013\Settings\CurrentSettings.vssettings"
+Write-Host ("Changing settings in '" + $settingsPath + "'")
+
+$xpathDoc = New-Object "System.Xml.XPath.XPathDocument" $settingsPath
+$xslTrans = New-Object "System.Xml.Xsl.XslCompiledTransform"
+$xslTrans.Load("vssettings-vs2013.xslt")
+$settings = New-Object "System.Xml.XmlWriterSettings"
+$settings.Indent = $true
+$settings.Encoding = [System.Text.Encoding]::UTF8
+$settings.OmitXmlDeclaration = $false
+
+$writer = [System.Xml.XmlWriter]::Create($settingsPath, $settings)
+$xslTrans.Transform($xpathDoc, $null, $writer)
+$writer.Close()
