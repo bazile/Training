1f93dada2da7fc7aa37e50fa4735e8b4f4b55a50
Vasily <vasily.petruhin@gmail.com>
2018-07-18 17:07:40 +0300

    Min/max stats for ThreadPool

--- CSharp/L08-S02-Threads/ThreadsDemo-01/Program.cs
+++ CSharp/L08-S02-Threads/ThreadsDemo-01/Program.cs
@@ -3,65 +3,75 @@ using System.Threading;
 
 namespace ThreadsDemo.ConsoleMessages
 {
-	class Program
-	{
-		static void Main()
-		{
-			var printMsgThread = new Thread(new ThreadStart(PrintMessage));
-			//printMsgThread.IsBackground = true;
-			printMsgThread.Name = "Поток #2";
-			printMsgThread.Start();
+    class Program
+    {
+        static void Main()
+        {
+            var printMsgThread = new Thread(new ThreadStart(PrintMessage));
+            //printMsgThread.IsBackground = true;
+            printMsgThread.Name = "Поток #2";
+            printMsgThread.Start();
 
-			//var printThreadWithParam = new Thread(new ParameterizedThreadStart(PrintMessage));
-			////printThreadWithParam.IsBackground = true;
-			//printThreadWithParam.Name = "Поток #3";
-			//printThreadWithParam.Start("Дополнительный поток с другим сообщением!");
+            //var printThreadWithParam = new Thread(new ParameterizedThreadStart(PrintMessage));
+            ////printThreadWithParam.IsBackground = true;
+            //printThreadWithParam.Name = "Поток #3";
+            //printThreadWithParam.Start("Дополнительный поток с другим сообщением!");
 
-			//var printThreadWithComplexParam = new Thread(new ParameterizedThreadStart(PrintComplexMessage));
-			////printThreadWithComplexParam.IsBackground = true;
-			////printThreadWithComplexParam.Start(new PrintMessageData { Message = "I'm super-parameterized thread!", When = new DateTime(2014, 3, 18) });
-			//printThreadWithComplexParam.Name = "Поток #4";
-			//printThreadWithComplexParam.Start("hggjdf");
+            //var printThreadWithComplexParam = new Thread(new ParameterizedThreadStart(PrintComplexMessage));
+            ////printThreadWithComplexParam.IsBackground = true;
+            ////printThreadWithComplexParam.Start(new PrintMessageData { Message = "I'm super-parameterized thread!", When = new DateTime(2014, 3, 18) });
+            //printThreadWithComplexParam.Name = "Поток #4";
+            //printThreadWithComplexParam.Start("hggjdf");
 
-			for (int i = 0; i < 100; i++)
-			{
-				Console.WriteLine("[{0:00}] Главный (UI) поток!", i);
-			}
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("[{0:00}] Главный (UI) поток!", i);
+            }
 
-			// Ожидаем завершения потоков
-			//printMsgThread.Join();
-			//printThreadWithParam.Join();
-		}
+            // Ожидаем завершения потоков
+            //printMsgThread.Join();
+            //printThreadWithParam.Join();
 
-		private static void PrintMessage()
-		{
-			for (int i = 0; i < 100; i++)
-			{
-				Console.WriteLine("[{0:00}] Дополнительный поток!", i);
-			}
-		}
+            #region ThreadPool
 
-		private static void PrintMessage(object text)
-		{
-			for (int i = 0; i < 100; i++)
-			{
-				Console.WriteLine("[{0:00}] {1}", i, text);
-			}
-		}
+            //int minWorkerThreads, minCompletionPortThreads, maxWorkerThreads, maxCompletionPortThreads;
+            //ThreadPool.GetMinThreads(out minWorkerThreads, out minCompletionPortThreads);
+            //ThreadPool.GetMaxThreads(out maxWorkerThreads, out maxCompletionPortThreads);
+            //Console.WriteLine("Количество рабочих потоков     : {0}..{1}", minWorkerThreads, maxWorkerThreads);
+            //Console.WriteLine("Количество потоков ввода/вывода: {0}..{1}", minCompletionPortThreads, maxCompletionPortThreads);
 
-		private static void PrintComplexMessage(object obj)
-		{
-			var data = (PrintMessageData)obj;
-			for (int i = 0; i < 100; i++)
-			{
-				Console.WriteLine("[{0:00}] {1:g} - {2}", i, data.When, data.Message);
-			}
-		}
+            #endregion
+        }
 
-		private class PrintMessageData
-		{
-			public string Message;
-			public DateTime When;
-		}
-	}
+        private static void PrintMessage()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("[{0:00}] Дополнительный поток!", i);
+            }
+        }
+
+        private static void PrintMessage(object text)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("[{0:00}] {1}", i, text);
+            }
+        }
+
+        private static void PrintComplexMessage(object obj)
+        {
+            var data = (PrintMessageData)obj;
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("[{0:00}] {1:g} - {2}", i, data.When, data.Message);
+            }
+        }
+
+        private class PrintMessageData
+        {
+            public string Message;
+            public DateTime When;
+        }
+    }
 }
