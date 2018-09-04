381f404373b3b5df9d6912dce5080b25508f8f8c
Vasily <vasily.petruhin@gmail.com>
2018-09-04 16:24:01 +0300

    Updated generic methods demo

--- CSharp/L02-S05-Generics/GenericsDemo/Program.cs
+++ CSharp/L02-S05-Generics/GenericsDemo/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 
 namespace GenericsDemo
@@ -7,14 +7,35 @@ namespace GenericsDemo
     {
         static void Main(string[] args)
         {
+            #region Обычный вариант
+
             int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            ArrayHelper.Shuffle<int>(numbers);
+            ArrayHelper.Shuffle(numbers);
+            Console.WriteLine(string.Join("; ", numbers));
 
             string[] letters = { "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й" };
-            ArrayHelper.Shuffle<string>(letters);
+            ArrayHelper.Shuffle(letters);
+            Console.WriteLine(string.Join("; ", letters));
 
-            PlayingCard[] deck = Deck.Get36CardDeck();
-            ArrayHelper.Shuffle<PlayingCard>(deck);
+            //PlayingCard[] deck = Deck.Get36CardDeck();
+            //ArrayHelper.Shuffle(deck);
+
+            #endregion
+
+            #region Вариант с обобщенной реализацией
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //ArrayHelper.Shuffle<int>(numbers);
+            //Console.WriteLine(string.Join("; ", numbers));
+
+            //string[] letters = { "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й" };
+            //ArrayHelper.Shuffle<string>(letters);
+            //Console.WriteLine(string.Join("; ", letters));
+
+            //PlayingCard[] deck = Deck.Get36CardDeck();
+            //ArrayHelper.Shuffle<PlayingCard>(deck);
+
+            #endregion
         }
     }
 }
